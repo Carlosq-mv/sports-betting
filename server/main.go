4a59d9ebbdd4f3f4d8bb08a4121e208d3256acf7
Carlos Quiroz <cquiroz7869@gmail.com>
2024-07-19 17:38:47 -0700

    feat: main file

---
+++ server/main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"log/slog"
+	"net/http"
+	"os"
+
+	"github.com/carlosq-mv/moist-sports/handlers"
+	"github.com/carlosq-mv/moist-sports/pkg/db"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/cors"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	if err := initEverything(); err != nil {
+		log.Fatal(err)
+	}
+
+	// _, err := db.DB.NewCreateTable().
+	// 	Model((*types.User)(nil)).
+	// 	Exec(context.Background())
+	// if err != nil {
+	// 	panic(err)
+	// }
+
+	router := chi.NewMux()
+	corsHandler := cors.New(cors.Options{
+		AllowedOrigins:   []string{fmt.Sprintf("https://%s", os.Getenv("ACCEPTED_URL"))},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: true,
+		MaxAge:           300,
+	})
+	router.Use(corsHandler.Handler)
+
+	// routes
+	router.Post("/signup", handlers.HandleSignup)
+	router.Post("/login", handlers.HandleLogin)
+
+	// routes that use jwt authentication
+	router.Group(func(auth chi.Router) {
+		auth.Use(handlers.JwtMiddleware)
+		auth.Get("/user", handlers.GetUser)
+
+	})
+
+	listenAddr := os.Getenv("LISTEN_ADDR")
+	slog.Info("HTTP Server started", "listenAddr", listenAddr)
+	http.ListenAndServe(listenAddr, router)
+}
+
+func initEverything() error {
+	if err := godotenv.Load(); err != nil {
+		return err
+	}
+	return db.Init()
+}
