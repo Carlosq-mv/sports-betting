15223b867d2c29622e30fae6b9ff7c86518e3c09
Carlos Quiroz <cquiroz7869@gmail.com>
2024-07-19 17:38:05 -0700

    feat: user database model

---
+++ server/types/user.go
@@ -0,0 +1,21 @@
+package types
+
+import (
+	"time"
+
+	"github.com/uptrace/bun"
+)
+
+type User struct {
+	bun.BaseModel `bun:"table:users,alias:u"`
+
+	Id        int64     `bun:"id,pk,autoincrement" json:"id"`
+	FirstName string    `bun:"first_name,notnull" json:"first_name"`
+	LastName  string    `bun:"last_name,notnull" json:"last_name"`
+	Username  string    `bun:"username,unique,notnull" json:"username"`
+	Email     string    `bun:"email,unique,notnull" json:"email"`
+	Password  string    `bun:"password,notnull" json:"password"`
+	LoggedIn  bool      `bun:"logged_in,notnull" json:"logged_in"`
+	CreatedAt time.Time `bun:"created_at,notnull,default:current_timestamp" json:"created_at"`
+	UpdatedAt time.Time `bun:"updated_at,notnull,default:current_timestamp" json:"updated_at"`
+}
