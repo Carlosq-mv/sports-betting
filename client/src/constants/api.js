947f2347de80d2bb5ac3ed90dccce4997b1635ba
Carlos Quiroz <cquiroz7869@gmail.com>
2024-09-08 01:08:54 -0700

    removed interceptor for auth header

--- client/src/constants/api.js
+++ client/src/constants/api.js
@@ -6,18 +6,5 @@ const AxiosInstance = axios.create({
     baseURL: baseURL,
 });
 
-AxiosInstance.interceptors.request.use(
-    async (config) => {
-        const token = localStorage.getItem("jwtToken");
-        if (token) {
-            config.headers.Authorization = `Bearer ${token}`
-        }
-        // console.log(config);
-        return config;
-    },
-    (error) => {
-        return Promise.reject(error);
-    },
-);
 
 export default AxiosInstance;
